a76938edc9eb7760176e641a1a6d16587de60f10
sunho45 <skyeee25@naver.com>
2025-07-01 13:48:12 +0900

    maxheap,minheap

--- heap.cpp
+++ heap.cpp
@@ -7,10 +7,12 @@ class node{
     public:
     node*left;
     node* right;
+    node* parent;
     int *num;
     node(int *num){
         left=nullptr;
         right=nullptr;
+        parent=nullptr;
         this->num=num;
     
     }
@@ -18,7 +20,7 @@ class node{
 
 
 
-class heap{
+class heap{       /* maxheap*/
     public:
      node *root;
         queue <node*> q;
@@ -31,7 +33,7 @@ class heap{
      bool isempty(){
         return root==nullptr;
         }
-        void push(int *num){
+        void pushmax(int *num){
             node *newnode=new node(num);
         if(isempty()){
             
@@ -41,13 +43,18 @@ class heap{
         else{
            node *current=q.front();
             if(q.front()->left==nullptr){
-                current->left=new node(num);
+                current->left=newnode;
+                newnode->parent=current;
                 q.push(current->left);
+                heapup(current->left);
             }
             else if(q.front()->right==nullptr){
-                current->right=new node(num);
+                current->right=newnode;
                 q.push(current->right);
+                newnode->parent=current;
+                heapup(current->right);
                 q.pop();
+
             }
 
            
@@ -56,6 +63,74 @@ class heap{
         
         }
         }
+
+
+         void pushmin(int *num){
+            node *newnode=new node(num);
+        if(isempty()){
+            
+            root=newnode;
+            q.push(root);
+        }
+        else{
+           node *current=q.front();
+            if(q.front()->left==nullptr){
+                current->left=newnode;
+                newnode->parent=current;
+                q.push(current->left);
+                heapupmin(current->left);
+            }
+            else if(q.front()->right==nullptr){
+                current->right=newnode;
+                q.push(current->right);
+                newnode->parent=current;
+                heapupmin(current->right);
+                q.pop();
+
+            }
+
+           
+
+
+        
+        }
+        }
+        void heapup(node *childrennode){
+            node* current =childrennode;
+            
+            
+            while(current->parent!=nullptr&&*(current->num)>*(current->parent->num)){
+                swap(current->num,current->parent->num);
+                current=current->parent;
+
+            }
+
+
+        }
+
+         void heapupmin(node *childrennode){
+            node* current =childrennode;
+            
+            
+            while(current->parent!=nullptr&&*(current->num)<*(current->parent->num)){
+                swap(current->num,current->parent->num);
+                current=current->parent;
+
+            }
+
+
+        }
+        void swap(int *a, int *b){
+
+            int temp=*a;
+            *a=*b;
+            *b=temp;
+
+
+        }
+
+
+
 
         void levelorder(){
             queue <node*> t;
@@ -107,11 +182,11 @@ heap *hp=new heap();
 int a=10;
 int b=40;
 int c=20;
-int d=0;
-hp->push(&a);
-hp->push(&b);
-hp->push(&c);
-hp->push(&d);
+int d=50;
+hp->pushmin(&a);
+hp->pushmin(&b);
+hp->pushmin(&c);
+hp->pushmin(&d);
 hp->levelorder();
 
 
